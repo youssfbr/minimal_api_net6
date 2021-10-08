60ee84ca6a169403db201e7e6d6324af833779e6
Alisson Youssf <youssfbr@gmail.com>
2021-10-07 23:20:16 -0300

    Models

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => {
+    var todo = new Todo(Guid.NewGuid(), "Ir à academia", true);
+    return Results.Ok(todo);
+});
 
 app.Run();
