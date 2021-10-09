5c082131353e75aa6288a45a7217d17a1214e2cc
Alisson Youssf <youssfbr@gmail.com>
2021-10-09 14:46:06 -0300

    Validation, Context, Migration, POST

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
+using MiniTodo.Data;
+using MiniTodo.ViewModels;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => {
-    var todo = new Todo(Guid.NewGuid(), "Ir à academia", true);
-    return Results.Ok(todo);
+app.MapGet("v1/todos", (AppDbContext context) =>
+{
+    var todos = context.Todos.ToList();
+    return Results.Ok(todos);
+});
+
+app.MapPost("v1/todos", (
+    AppDbContext context,
+    CreateTodoViewModel model) =>
+{
+
+    var todo = model.MapTo();
+    if (!model.IsValid)
+        return Results.BadRequest(model.Notifications);
+
+    context.Todos.Add(todo);
+    context.SaveChanges();
+
+    return Results.Created($"/vi/todos/{todo.Id}", todo);
 });
 
 app.Run();
